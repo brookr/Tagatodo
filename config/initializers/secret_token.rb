7543231d0f2d741664577e1f9aa5f0c45ba89489
Danny Tatom <dannytatom@gmail.com>
2012-08-08 12:48:30 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tagatodo::Application.config.secret_token = 'cd95b5ddd821ed676ba3195af6c3166801578b18272add9a8b9ee9e31ad6837af64618a51ab48fbf09bc54baa8481786b879f20e434cb399238fe32a972109ad'
